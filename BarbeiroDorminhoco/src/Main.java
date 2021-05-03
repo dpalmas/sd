435b81d7620725280a232527f8d3137ecea63f8c
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-05-02 23:45:49 -0300

    Update Main.java

--- BarbeiroDorminhoco/src/Main.java
+++ BarbeiroDorminhoco/src/Main.java
@@ -1,10 +1,5 @@
 import java.util.ArrayList;
 
-/**
- * @author davi on 4/19/21
- * @project BarbeiroDorminhoco
- */
-
 public class Main {
 
     public static ArrayList<Cadeira> cadeiras = new ArrayList<>();
@@ -12,14 +7,6 @@ public class Main {
     public static void main(String[] args) 
     {
         BarbeiroDorminhoco barbeiroDorminhoco = new BarbeiroDorminhoco();
-
-        // int nCadeiras = 3;
-        // int nClientes = 5;
-
-        // for (int i = 0; i < nCadeiras; i++) {
-        //     Cadeira cadeira = new Cadeira();
-        //     cadeiras.add(cadeira);
-        // }
 
         Barbeiro barbeiroThread = new Barbeiro(barbeiroDorminhoco);
         Cliente clienteThread1 = new Cliente(barbeiroDorminhoco, 1);
