080f0f036a6a71f4c0fc13963c207b052cb06e8c
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-05-25 20:38:30 -0300

    Cleaning

--- Bar/src/Bar.java
+++ Bar/src/Bar.java
@@ -1,72 +1,93 @@
-package src;
+import java.util.concurrent.LinkedBlockingQueue;
 
-import java.util.LinkedList;
-import java.util.Queue;
+public class Bar extends Thread 
+{
+    LinkedBlockingQueue<Waiter> waitersQueue = new LinkedBlockingQueue<>();
 
-/**
- * @author davi on 5/5/21
- * @project Bar
- */
+    boolean waiteresAreWaiting = true; 
+    boolean barIsOpen = true; 
+    int rounds = 5; 
 
-public class Bar {
-    int numClientes;
-    int numGarcons;
-    int capGarcons;
-    int numRodadas;
-    int rodada;
-    int sendoAtendidos;
+    @Override
+    public void run() 
+    {
+        while(barIsOpen)
+        {   
+            try 
+            {
+                RoundsController();
+                VerifyWaiters();
+                
+                if(waiteresAreWaiting)
+                {
+                    rounds--;
+                    System.out.println(rounds);
+                    waiteresAreWaiting = false;    
+                    RunAnotherRound();                 
+                }
+            } 
+            catch (Exception e) 
+            {
 
-    boolean fechouBar = true;
-    boolean existemClientesNoBar = false;
-    boolean recebendoPedidos = false;
-    Queue<Cliente> clientes = new LinkedList<>();
-    Queue<Garcom> garcons = new LinkedList<>();
-    Queue<Thread> threads = new LinkedList<>();
+            }   
+        } 
+    }
 
+    public void VerifyWaiters()
+    {
+        for (Waiter waiter : waitersQueue) 
+        {
+            if (waiter.GetIsWaitingForNextRound()) 
+            { 
+                continue; 
+            }
+            else
+            {
+                return; 
+            }
 
-    Bar(int numClientes, int numGarcons, int capGarcons, int numRodadas) {
-        this.numClientes = numClientes;
-        this.numGarcons = numGarcons;
-        this.capGarcons = capGarcons;
-        this.numRodadas = numRodadas;
-        this.rodada = 1;
-
-        for (int i = 1; i <= numGarcons; i++) {
-            garcons.add(new Garcom(this, "Garçom" + i));
-            System.out.println("Garçom" + i + " estará a trabalho hoje.");
         }
+        waiteresAreWaiting = true; 
+    }
 
-        for (int i = 1; i <= numClientes; i++) {
-            clientes.add(new Cliente(this, "Cliente" + i));
-            System.out.println("Cliente" + i + " estará no buteco.");
-        }
-
-        for (Garcom garcom : garcons) {
-            threads.add(new Thread(garcom, garcom.nome));
-        }
-
-        for (Cliente cliente : clientes) {
-            threads.add(new Thread(cliente, cliente.nome));
+    public void RunAnotherRound()
+    {
+        for (Waiter waiter : waitersQueue) 
+        {
+            waiter.run();
         }
     }
 
-    public static void main(String[] args) {
-        int clientes = 5;
-        int garcons = 2;
-        int capacidadeGarcons = 3;
-        int rodadas = 10;
-        Bar bar = new Bar(clientes, garcons, capacidadeGarcons, rodadas);
-        bar.iniciaCorreria();
-    }
-
-    public void iniciaCorreria() {
-        System.out.println("O bar está oficialmente aberto!");
-        recebendoPedidos = true;
-        this.fechouBar = false;
-        this.existemClientesNoBar = true;
-
-        for (Thread thread : threads) {
-            thread.start();
+    public void RoundsController()
+    {
+        while(barIsOpen)
+        {
+            if(rounds <= 0)
+            {
+                barIsOpen = false; 
+            }
         }
+        
     }
+
+    public boolean GetBarIsOpen()
+    {
+        return barIsOpen;
+    }
+
+    // int clientCount = 20;
+    // int waiterCount = 2;
+
+    // Waiter waiter = new Waiter();
+    // Waiter waiter2 = new Waiter();
+
+    // void WaiterAttendanceCapacity()
+    // {
+        // Acapacidade de atendimento dos garçon = vai ser o numero de clientes divido
+    // pela numero de garçons
+    //     int attendenceCapacity = clientCount / waiterCount;
+
+    //     // para cada garçom 
+    //         // adiciona a capacidade de atendimento 
+    // }
 }
