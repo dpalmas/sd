e9a34ad6acc89261284d46e781278b65a620084e
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-08-16 10:38:40 -0300

    Update Cliente.java

--- Sockets/Exercicio2/src/Cliente.java
+++ Sockets/Exercicio2/src/Cliente.java
@@ -4,12 +4,12 @@ import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Cliente {
-    public static void main(String[] args) throws IOException {
-        // Cria o socket com o recurso desejado na porta especificada
+public class Cliente 
+{
+    public static void main(String[] args) throws IOException 
+    {    
         Socket s = new Socket("127.0.0.1", 7000);
 
-        // Cria a Stream de saida de dados
         DataInputStream in = new DataInputStream(s.getInputStream());
         DataOutputStream ps = new DataOutputStream(s.getOutputStream());
 
@@ -17,13 +17,13 @@ public class Cliente {
         System.out.println(in.readUTF());
 
         String str = "";
-        do {
+        do 
+        {
             str = teclado.nextLine();
             ps.writeUTF(str);
             System.out.println(in.readUTF());
         } while (!str.equals("tchau"));
 
-        // Encerra o socket cliente
         s.close();
     }
 }
