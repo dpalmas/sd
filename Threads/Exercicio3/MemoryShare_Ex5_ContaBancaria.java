ba032e22bb435041a9d18aaed6161d4db3b6acda
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-08-16 14:16:46 -0300

    Update MemoryShare_Ex5_ContaBancaria.java

--- Threads/Exercicio3/MemoryShare_Ex5_ContaBancaria.java
+++ Threads/Exercicio3/MemoryShare_Ex5_ContaBancaria.java
@@ -1,66 +1,77 @@
-
-class Conta {
+class Conta
+{
     int saldo = 0;
 
 }
 
-class Retirada extends Thread {
+class Retirada extends Thread
+{
     private Conta c;
 
-    public Retirada(Conta c){
-     this.c = c;
+    public Retirada(Conta c)
+    {
+        this.c = c;
     }
 
-
-    public void run() {
-        int vlrs[] = { 110, 20, 30, 40, 50, 60 };
-        synchronized (c){
-            try {
-                for (int i = 0; i < vlrs.length; i++) {
-
-                        c.saldo -= vlrs[i];
-                        System.out.println("Retirada: " + vlrs[i] + " - Saldo: " + c.saldo);
-
+    public void run()
+    {
+        int vlrs[] =
+        { 110, 20, 30, 40, 50, 60 };
+        synchronized (c)
+        {
+            try
+            {
+                for (int i = 0; i < vlrs.length; i++)
+                {
+                    c.saldo -= vlrs[i];
+                    System.out.println("Retirada: " + vlrs[i] + " - Saldo: " + c.saldo);
                 }
-
-
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
             }
         }
-
 
     }
 }
 
- class Deposito extends Thread {
+class Deposito extends Thread
+{
     private Conta c;
 
-     public Deposito(Conta c) {
+    public Deposito(Conta c)
+    {
+        this.c = c;
+    }
 
-         this.c = c;
-     }
-
-
-    public void run() {
-        int vlrs[] = { 40, 50, 60, 10, 20, 30 };
-        synchronized (c){
-            try {
-                for (int i = 0; i < vlrs.length; i++) {
+    public void run()
+    {
+        int vlrs[] =
+        { 40, 50, 60, 10, 20, 30 };
+        synchronized (c)
+        {
+            try
+            {
+                for (int i = 0; i < vlrs.length; i++)
+                {
                     c.saldo += vlrs[i];
                     System.out.println("Deposito: " + vlrs[i] + " - Saldo: " + c.saldo);
-
                 }
 
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
             }
         }
-
-
     }
 }
 
-public class MemoryShare_Ex5_ContaBancaria {
-    public static void main(String[] args) {
+public class MemoryShare_Ex5_ContaBancaria
+{
+    public static void main(String[] args)
+    {
         Conta c = new Conta();
 
         Thread d = new Deposito(c);
@@ -69,10 +80,15 @@ public class MemoryShare_Ex5_ContaBancaria {
         d.start();
         r.start();
 
-        try {
+        try
+        {
             d.join();
             r.join();
-        } catch (InterruptedException e) { }
+        }
+        catch (InterruptedException e)
+        {
+            System.out.println("Target thread was interrupted");
+        }
 
         System.out.println("Saldo=" + c.saldo);
     }
