eb2bb457439b359eaf2c674f06c5a942564efbc5
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-08-16 14:07:09 -0300

    Update Bar.java

--- Threads/Exercicio1/Bar/src/Bar.java
+++ Threads/Exercicio1/Bar/src/Bar.java
@@ -1,58 +1,57 @@
 import java.util.concurrent.LinkedBlockingQueue;
 
-public class Bar extends Thread 
+public class Bar extends Thread
 {
     LinkedBlockingQueue<Waiter> waitersQueue = new LinkedBlockingQueue<>();
 
-    boolean waiteresAreWaiting = true; 
-    boolean barIsOpen = true; 
-    int rounds = 5; 
+    boolean waiteresAreWaiting = true;
+    boolean barIsOpen = true;
+    int rounds = 5;
 
     @Override
-    public void run() 
+    public void run()
     {
-        while(barIsOpen)
-        {   
-            try 
+        while (barIsOpen)
+        {
+            try
             {
                 RoundsController();
                 VerifyWaiters();
-                
-                if(waiteresAreWaiting)
+
+                if (waiteresAreWaiting)
                 {
                     rounds--;
                     System.out.println(rounds);
-                    waiteresAreWaiting = false;    
-                    RunAnotherRound();                 
+                    waiteresAreWaiting = false;
+                    RunAnotherRound();
                 }
-            } 
-            catch (Exception e) 
+            }
+            catch (Exception e)
             {
-
-            }   
-        } 
+                e.printStackTrace();
+            }
+        }
     }
 
     public void VerifyWaiters()
     {
-        for (Waiter waiter : waitersQueue) 
+        for (Waiter waiter : waitersQueue)
         {
-            if (waiter.GetIsWaitingForNextRound()) 
-            { 
-                continue; 
+            if (waiter.GetIsWaitingForNextRound())
+            {
+                continue;
             }
             else
             {
-                return; 
+                return;
             }
-
         }
-        waiteresAreWaiting = true; 
+        waiteresAreWaiting = true;
     }
 
     public void RunAnotherRound()
     {
-        for (Waiter waiter : waitersQueue) 
+        for (Waiter waiter : waitersQueue)
         {
             waiter.run();
         }
@@ -60,34 +59,17 @@ public class Bar extends Thread
 
     public void RoundsController()
     {
-        while(barIsOpen)
+        while (barIsOpen)
         {
-            if(rounds <= 0)
+            if (rounds <= 0)
             {
-                barIsOpen = false; 
+                barIsOpen = false;
             }
         }
-        
     }
 
     public boolean GetBarIsOpen()
     {
         return barIsOpen;
     }
-
-    // int clientCount = 20;
-    // int waiterCount = 2;
-
-    // Waiter waiter = new Waiter();
-    // Waiter waiter2 = new Waiter();
-
-    // void WaiterAttendanceCapacity()
-    // {
-        // Acapacidade de atendimento dos garçon = vai ser o numero de clientes divido
-    // pela numero de garçons
-    //     int attendenceCapacity = clientCount / waiterCount;
-
-    //     // para cada garçom 
-    //         // adiciona a capacidade de atendimento 
-    // }
 }
